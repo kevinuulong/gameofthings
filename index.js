d24f82fc2b43f885e57bc10c19acf690939b08f4
Kevin Long <kevin.uu.long@gmail.com>
2019-09-03 22:58:21 -0400

    added heroku support

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     app = express(),
-    server = app.listen(5000, '0.0.0.0', function(){
+    server = app.listen(process.env.port || 5000, '0.0.0.0', function(){
         console.log("connected to port 5000")
     }),
     io = require('socket.io') (server)
@@ -102,4 +102,4 @@ io.on('connection', function(socket){
     });
 });
 
-server.listen(5000);
+server.listen(process.env.port || 5000);
