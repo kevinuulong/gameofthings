c639bbf7ca5b4e1c4a1906aba1cbcbd69ba2077e
Kevin Long <kevin.uu.long@gmail.com>
2020-04-10 14:36:38 -0400

    Testing redesign
    Complete UI Overhaul
    Version: 2.0.0-alpha
    
    TODO: Full Play Tests

--- public/client.js
+++ public/client.js
@@ -5,8 +5,8 @@ function remove(element){
 window.onload= function(){
 
     var socket = io('thegameofthings.herokuapp.com',{'forceNew':true}), 
-        host = document.getElementById("host-a-thing"),
-        join = document.getElementById("submit-id"), //probably not needed anymore
+        host = document.getElementById("hostthing"),
+        join = document.getElementById("join-button"),
         hostscreen = false;
 
     host.addEventListener("click", function(){
@@ -16,17 +16,13 @@ window.onload= function(){
         console.log("new game registered");
     });
 
-    document.getElementById("submit-thing-id").addEventListener("click", function(){
-        document.getElementById("thing-id-input-box").style.display = "none";
-        document.getElementById("submit-thing-id").style.display = "none";
-        document.getElementById("username-input-box").style.display = "block";
-        document.getElementById("submit-username").style.display = "inline-block";
-        document.getElementById("host-a-thing").style.display = "none";
+    join.addEventListener("click", function(){
+        document.getElementById("join-screen").style.display = "none";
     });
 
     socket.on('reloadCheck', function(){
-        var thingid = document.getElementById("thing-id-input-box").value,
-            username = document.getElementById("username-input-box").value;
+        var thingid = document.getElementById("thing-id").value,
+            username = document.getElementById("username").value;
 
         console.log('reloadCheck')
         if(thingid.length!=0&&username.length!=0){
@@ -38,24 +34,21 @@ window.onload= function(){
         
     })
 
-    document.getElementById("submit-username").addEventListener("click", function(){
-        var thingid = document.getElementById("thing-id-input-box").value;
+    document.getElementById("join-button").addEventListener("click", function(){
+        var thingid = document.getElementById("thing-id").value;
         socket.emit('joinGame', {
             room: thingid,
-            username: document.getElementById("username-input-box").value
+            username: document.getElementById("username").value
         });
-        document.getElementById("join-card").style.display = "none";
+        document.getElementById("join-screen").style.display = "none";
+        document.getElementById("player-screen").style.display = "grid";
     });
 
     socket.on('newGame', function(data){
-        document.getElementById("thing-id-input-box").style.display = "none";
-        document.getElementById("submit-thing-id").style.display = "none";
-        document.getElementById("host-a-thing").style.display = "none";
-        document.getElementById("title").innerHTML = data.game;
-        document.getElementById("explainer").style.display = "block";
-        document.getElementById("explainer").innerHTML = "to join enter the thing-id above and a username";
-        document.getElementById("start-game").style.display = "inline-block";
-        document.getElementById("players").style.display = "flex";
+        document.getElementById("join-screen").style.display = "none";
+        document.getElementById("host-screen").style.display = "grid";
+        document.getElementById("image0").setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', `https://quickchart.io/qr?text=http%3A%2F%2Fthegameofthings.herokuapp.com%2F%3Fid%3D${data.game}`);
+        document.getElementById("thing-id-host").innerHTML = data.game;
         hostscreen = true;
     });
 
@@ -64,64 +57,96 @@ window.onload= function(){
     });
 
     socket.on('newUser', function(data){
-        //alert(data.username + " joined");
-        document.getElementById("players").innerHTML += "<div>" + data.username + "</div>";
+        document.getElementById("playerlist").innerHTML += "<li class='needsupdate'>" + data.username + "</li>";
+        document.getElementsByClassName("needsupdate")[document.getElementsByClassName("needsupdate").length-1].style.background = randomcolor();
     });
 
-    document.getElementById("start-game").addEventListener("click", function(){
+    document.getElementById("start-button").addEventListener("click", function(){
         socket.emit('startGame', {
-            room: document.getElementById("title").innerHTML
+            room: document.getElementById("thing-id-host").innerHTML
         });
-        document.getElementById("join-card").style.display = "none";
+        //Don't think this is necessary anymore: document.getElementById("join-card").style.display = "none";
     });
 
-    document.getElementById("newthing").addEventListener("click", function(){
+    document.getElementById("shuffle").addEventListener("click", function(){
         socket.emit('startGame', {
-            room: document.getElementById("title").innerHTML
+            room: document.getElementById("thing-id-host").innerHTML
         });
-        document.getElementById("join-card").style.display = "none";
+        //Don't think this is necessary anymore: document.getElementById("join-card").style.display = "none";
     });
 
     socket.on('newThing', function(data){
         if (hostscreen){
-            document.getElementById("hostscreen").style.display = "block";
-            document.getElementById("hostthing").innerHTML = data.thing;
+            document.getElementById("host-screen").style.display = "none";
+            document.getElementById("thing-screen").style.background = randomcolor();
+            document.getElementById("thing-screen").style.display = "block";
+            document.getElementById("thing").innerHTML = data.thing;
+            document.getElementById("thing-results").innerHTML = data.thing;
         } else{
-            document.getElementById("playerscreen").style.display = "block";
-            document.getElementById("playerresponseinputbox").value = "";
+            document.getElementById("thing-player").innerHTML = data.thing;
+            document.getElementById("player-screen").style.display = "flex";
+            document.getElementById("waiting").style.display = "none";
+            document.getElementById("player-screen").style.background = randomcolor();
+            document.getElementById('player-thing-dialog').style.display = "block";
+            document.getElementById("response-box").value = "";
         }
     });
 
-    document.getElementById("submitresponse").addEventListener("click", function(){
-        socket.emit('receiveResponse', {
-            response: document.getElementById("playerresponseinputbox").value,
-            room: document.getElementById("thing-id-input-box").value
-        });
-        document.getElementById("playerscreen").style.display = "none";
-        document.getElementById("playerresponsecount").style.display = "block";
+    document.getElementById("resend").addEventListener("click", function(){
+        socket.emit('resendrequest', {
+            thing: document.getElementById("thing").innerHTML,
+            room: document.getElementById("thing-id-host").innerHTML
+        })
+    });
+
+    socket.on('resend', function(data){
+        if (!hostscreen){
+            document.getElementById("thing-player").innerHTML = data.thing;
+            document.getElementById("player-screen").style.display = "flex";
+            document.getElementById("waiting").style.display = "none";
+            document.getElementById("count").style.display = "none";
+            document.getElementById("player-screen").style.background = randomcolor();
+            document.getElementById('player-thing-dialog').style.display = "block";
+            document.getElementById("response-box").value = "";
+        }
+    });
+
+    document.getElementById("response-box").addEventListener("keydown", function(e){
+        if(e.keyCode == 13){
+            socket.emit('receiveResponse', {
+                response: document.getElementById("response-box").value,
+                room: document.getElementById("thing-id").value
+            });
+            document.getElementById("player-thing-dialog").style.display = "none";
+            document.getElementById("count").style.display = "block";
+        }
+        
     });
 
     socket.on('responsecount', function(data){
         if (data.count == 1){
-            document.getElementById("playerresponsecount").innerHTML = data.count + ' thing received';
+            document.getElementById("count").innerHTML = data.count + ' thing submitted';
         } else{
-            document.getElementById("playerresponsecount").innerHTML = data.count + ' things received';
+            document.getElementById("count").innerHTML = data.count + ' things submitted';
         }
         
     });
 
     socket.on('displayresponses', function(data){
         var responses = data.shuffled;
-        document.getElementById("playerresponsecount").style.display = "none";
+        document.getElementById("thing-screen").style.display = "none";
+        document.getElementById("things-screen").style.display = "block";
+        document.getElementById("count").style.display = "none";
         console.log(responses);
         for (i = 0; i < responses.length; i++){
-            document.getElementById("flex-container-host").innerHTML += "<div onclick='remove(this);'>"+ responses[i]+"</div>";
+            document.getElementById("responses").innerHTML += "<h1 class='answer'><span class='needsupdate' onclick='remove(this);'>"+ responses[i]+"</span></h1>";
+            document.getElementsByClassName("needsupdate")[document.getElementsByClassName("needsupdate").length-1].style.background = randomcolor();
         }
     });
 
-    document.getElementById("revealresponses").addEventListener("click", function(){
+    document.getElementById("reveal").addEventListener("click", function(){
         socket.emit('revealresponses', {
-            room: document.getElementById("title").innerHTML
+            room: document.getElementById("thing-id-host").innerHTML
         });
     });
 };
